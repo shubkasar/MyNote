222f0629fc2780bd7b759ea8bc95ce0c6cac4409
Shubham Milind Kasar <144850647+shubkasar@users.noreply.github.com>
2024-04-18 13:54:03 -0400

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EditNoteIcon from '@mui/icons-material/EditNote';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EditNoteIcon /> MyNote
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
